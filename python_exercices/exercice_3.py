3bfa73bc9abdc3135c47d8612d22669bfb091b2c
pyEdwin <cyredwin1@gmail.com>
2023-05-25 18:46:16 -0400

    write exercice_3.py

---
+++ python_exercices/exercice_3.py
@@ -0,0 +1,16 @@
+users = []
+
+users.append("Kevin")
+users.append("Bob")
+users.append("Alice")
+del users[1]
+users.reverse()
+users.insert(1 , "Melody")
+users += ["Pierre" , "Mike"]
+new_users = users[0:1]
+
+
+print(users)
+print(new_users)
+#print(reversed_users)
+
